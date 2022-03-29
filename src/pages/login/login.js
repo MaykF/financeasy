14a7ff077d82aa61d6bb396c858723f7c8ea4398
Maycon Ferrari Varella <mayconfvarella@hotmail.com>
2022-03-29 17:04:35 -0300

    preparação da tela de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './login.css'
+import logo from '../../icons/icone-login.png'
+import senha from '../../icons/icone-senha.png'
 
 export default function login(){
     return (
         <div className='login'>
             <div className='logo'>
-                teste
-                <img src="C:\Users\Maycon\Downloads\icons8-login-64.png" alt=""/>
+                <img src={logo} alt=""/>
+            </div>
+            <div className='senha'>
+                <img src={senha} alt=""/>
             </div>
         </div>
     )
