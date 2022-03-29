51178e5b82d61ba7d0dbb549527d12bfc412d262
Maycon Ferrari Varella <mayconfvarella@hotmail.com>
2022-03-29 19:12:59 -0300

    criação e design do login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import './login.css'
-import logo from '../../icons/icone-login.png'
-import senha from '../../icons/icone-senha.png'
+import logo from '../../icons/imagem-login2.png'
+import { MdEmail, MdLock } from "react-icons/md";
+import { HiEye, HiEyeOff} from "react-icons/hi";
 
 export default function login(){
     return (
         <div className='login'>
-            <div className='logo'>
+
+            <div className='login-logo'>
                 <img src={logo} alt=""/>
             </div>
-            <div className='senha'>
-                <img src={senha} alt=""/>
+
+            <div className='login-right'>
+                <h2>Financeasy</h2>
+
+                <div className='loginInputEmail'>
+                    <MdEmail/>
+                    <input type="text"
+                    placeholder="Digite seu email"
+                    />
+                </div>
+
+                <div className="loginInputPassword">
+                    <MdLock />
+                    <input  
+                        type="text"
+                        placeholder='Digite sua senha'
+                    />
+                </div>
+
+                
+                <button type="submit">
+                    Entrar
+                </button>
+
+                <h4>Não tem conta?</h4>
+
+                <button type='submit'>
+                    Cadastrar
+                </button>
+                
             </div>
         </div>
     )
